0da14990d4bbebef4379993dd1783ca9599df96d
mateusz <siiikor.sikorski@gmail.com>
2024-09-19 21:58:28 +0200

    Improve the test.

--- internal/transfer/message_test.go
+++ internal/transfer/message_test.go
@@ -14,21 +14,22 @@ func Test_sendMessage(t *testing.T) {
 	holder := message.NewPutFileRequestHolder("huge_file_name", 404)
 	sizeBuffer := make([]byte, 12)
 	messageBuffer := bytes.NewBuffer(make([]byte, 0, 1024))
-	var socket io.Writer = bytes.NewBuffer(make([]byte, 0, 1024))
+	buffer := bytes.NewBuffer(make([]byte, 0, 1024))
 
-	err := sendMessage(socket, sizeBuffer, messageBuffer, &holder)
+	var sendSocket io.Writer = buffer
+	err := sendMessage(sendSocket, sizeBuffer, messageBuffer, &holder)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	messageBuffer.Reset()
 
+	var readSocket io.Reader = buffer
 	ctx := context.Background()
-	out, err := receiveMessage(ctx, socket.(io.Reader), messageBuffer)
+	out, err := receiveMessage(ctx, readSocket, messageBuffer)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%v", out)
 
 	payload := out.PayloadStruct
 	switch v := payload.(type) {
