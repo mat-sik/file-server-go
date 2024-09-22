12837faacebcef7dadca73f95aebd7d67f772f81
mateusz <siiikor.sikorski@gmail.com>
2024-09-22 19:30:19 +0200

    Implement request methods.

--- internal/client/service/service.go
+++ internal/client/service/service.go
@@ -1,106 +1,46 @@
 package service
 
 import (
-	"bytes"
-	"context"
-	"errors"
-	"fmt"
 	"github.com/mat-sik/file-server-go/internal/message"
-	"github.com/mat-sik/file-server-go/internal/transfer"
 	"io"
 	"os"
 )
 
-func SendRequest(
-	writer io.Writer,
-	headerBuffer []byte,
-	messageBuffer *bytes.Buffer,
-	req *message.Holder,
-) error {
-	defer messageBuffer.Reset()
-
-	if err := transfer.SendMessage(writer, headerBuffer, messageBuffer, req); err != nil {
-		return err
-	}
-	return nil
+func HandleGetFileRequest(filename string) (message.Request, error) {
+	return &message.GetFileRequest{Filename: filename}, nil
 }
 
-func ReceiveResponse(
-	ctx context.Context,
-	reader io.Reader,
-	buffer *bytes.Buffer,
-) (message.Holder, error) {
-	defer buffer.Reset()
-
-	holder, err := transfer.ReceiveMessage(ctx, reader, buffer)
-	if err != nil {
-		return message.Holder{}, err
-	}
-	return holder, nil
-}
-
-func HandleGetFile(
-	ctx context.Context,
-	reader io.Reader,
-	buffer *bytes.Buffer,
-	filename string,
-) (message.Holder, error) {
-	defer buffer.Reset()
-
-	holder, err := transfer.ReceiveMessage(ctx, reader, buffer)
-	if err != nil {
-		return message.Holder{}, err
-	}
-
-	res, ok := holder.PayloadStruct.(*message.GetFileResponse)
-	if !ok {
-		return message.Holder{}, ErrUnexpectedResponse
-	}
-	if res.Status != 200 {
-		return message.Holder{}, fmt.Errorf("error, status code: %d", res.Status)
-	}
-
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
-	if err != nil {
-		return message.Holder{}, err
-	}
-
-	fileSize := res.Size
-	if err = transfer.Stream(ctx, reader, file, buffer, fileSize); err != nil {
-		return message.Holder{}, err
-	}
-	return holder, nil
-}
-
-func PutFileHandleRequest(
-	ctx context.Context,
-	writer io.Writer,
-	headerBuffer []byte,
-	buffer *bytes.Buffer,
-	filename string,
-) error {
-	defer buffer.Reset()
-
+func HandlePutFileRequest(filename string) (message.Request, error) {
 	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	fileSize := int(fileInfo.Size())
 
-	holder := message.NewPutFileRequestHolder(filename, fileSize)
-	if err = SendRequest(writer, headerBuffer, buffer, &holder); err != nil {
-		return err
-	}
-	if err = transfer.Stream(ctx, file, writer, buffer, fileSize); err != nil {
-		return err
-	}
-	return nil
+	req := message.PutFileRequest{FileName: filename, Size: fileSize}
+	return &StreamRequest{
+		StructRequest: &req,
+		StreamWriter:  file,
+		ToTransfer:    fileSize,
+	}, nil
 }
 
-var ErrUnexpectedResponse = errors.New("unexpected response")
+func HandleDeleteFileRequest(filename string) (message.Request, error) {
+	return &message.DeleteFileRequest{FileName: filename}, nil
+}
+
+type StreamRequest struct {
+	StructRequest message.Request
+	StreamWriter  io.Writer
+	ToTransfer    int
+}
+
+func (req *StreamRequest) GetRequestType() message.TypeName {
+	return req.StructRequest.GetRequestType()
+}
