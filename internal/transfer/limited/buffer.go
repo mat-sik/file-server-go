3119736b0879e251be6feee2c0c962e0e382cf63
mateusz <siiikor.sikorski@gmail.com>
2024-10-04 21:34:50 +0200

    Implement custom buffer fit for the task.

---
+++ internal/transfer/limited/buffer.go
@@ -0,0 +1,111 @@
+package limited
+
+import (
+	"io"
+)
+
+type Buffer struct {
+	buffer []byte
+	offset int
+}
+
+func (b *Buffer) Reset() {
+	b.buffer = b.buffer[:0]
+	b.offset = 0
+}
+
+func (b *Buffer) empty() bool {
+	return len(b.buffer) == b.offset
+}
+
+func (b *Buffer) Len() int {
+	return len(b.buffer) - b.offset
+}
+
+func (b *Buffer) Cap() int {
+	return cap(b.buffer)
+}
+
+func (b *Buffer) Available() int {
+	return cap(b.buffer) - len(b.buffer)
+}
+
+func (b *Buffer) Write(p []byte) (int, error) {
+	toReserve := len(p)
+	oldLen, err := b.staticGrow(toReserve)
+	if err != nil {
+		return 0, err
+	}
+	availableBuffer := b.buffer[oldLen:]
+	return copy(availableBuffer, p), nil
+}
+
+func (b *Buffer) staticGrow(n int) (int, error) {
+	if n > b.Available() {
+		return 0, io.ErrShortBuffer
+	}
+	oldLen := len(b.buffer)
+	newLen := oldLen + n
+	b.buffer = b.buffer[:newLen]
+	return oldLen, nil
+}
+
+func (b *Buffer) WriteTo(w io.Writer) (int64, error) {
+	if b.empty() {
+		b.Reset()
+		return 0, nil
+	}
+	toWrite := b.buffer[b.offset:]
+	n, err := w.Write(toWrite)
+	b.offset += n
+	return int64(n), err
+}
+
+// MaxRead read as much as possible in a single cycle
+func (b *Buffer) MaxRead(r io.Reader) (int, error) {
+	if b.empty() {
+		b.Reset()
+	}
+	availableBuffer := b.buffer[b.offset:cap(b.buffer)]
+	n, err := r.Read(availableBuffer)
+	if err != nil {
+		return 0, err
+	}
+	b.buffer = availableBuffer[:n]
+	return n, nil
+}
+
+func (b *Buffer) Read(p []byte) (int, error) {
+	if b.empty() {
+		b.Reset()
+		if len(p) == 0 {
+			return 0, nil
+		}
+		return 0, io.EOF
+	}
+	toRead := b.buffer[b.offset:]
+	read := copy(p, toRead)
+	b.offset += read
+	return read, nil
+}
+
+func (b *Buffer) Next(n int) []byte {
+	if b.empty() {
+		b.Reset()
+		return nil
+	}
+	n = min(len(b.buffer), n)
+	data := b.buffer[b.offset : b.offset+n]
+	b.offset += n
+	return data
+}
+
+func (b *Buffer) Compact() {
+	unread := b.buffer[b.offset:]
+	b.Reset()
+	_, _ = b.Write(unread)
+}
+
+func NewBuffer(buffer []byte) *Buffer {
+	return &Buffer{buffer: buffer}
+}
