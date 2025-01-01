a8a617ef7174d2f997c8d28bc85556192e40231a
Mateusz <msikorski@griddynamics.com>
2025-01-01 15:47:23 +0100

    Create struct for common data across method for server router.

--- internal/server/router/router.go
+++ internal/server/router/router.go
@@ -15,23 +15,27 @@ import (
 	"time"
 )
 
-func HandleRequest(ctx context.Context, connCtx connection.Context) error {
-	req, err := receiveRequest(connCtx)
-	if err != nil {
-		return err
-	}
-
-	res, err := routeRequest(ctx, connCtx, req)
-	if err != nil {
-		return err
-	}
-
-	return deliverResponse(ctx, connCtx, res)
+type ServerRouter struct {
+	connection.Context
 }
 
-func receiveRequest(connCtx connection.Context) (message.Request, error) {
-	var reader io.Reader = connCtx.Conn
-	buffer := connCtx.Buffer
+func (serverRouter ServerRouter) HandleRequest(ctx context.Context) error {
+	req, err := serverRouter.receiveRequest()
+	if err != nil {
+		return err
+	}
+
+	res, err := serverRouter.routeRequest(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	return serverRouter.deliverResponse(ctx, res)
+}
+
+func (serverRouter ServerRouter) receiveRequest() (message.Request, error) {
+	var reader io.Reader = serverRouter.Conn
+	buffer := serverRouter.Buffer
 	m, err := transfer.ReceiveMessage(reader, buffer)
 	if err != nil {
 		return nil, err
@@ -44,8 +48,8 @@ func receiveRequest(connCtx connection.Context) (message.Request, error) {
 	return req, nil
 }
 
-func routeRequest(ctx context.Context, connCtx connection.Context, req message.Request) (message.Response, error) {
-	buffer := connCtx.Buffer
+func (serverRouter ServerRouter) routeRequest(ctx context.Context, req message.Request) (message.Response, error) {
+	buffer := serverRouter.Buffer
 	defer buffer.Reset()
 
 	ctx, cancel := context.WithTimeout(ctx, timeForRequest)
@@ -57,7 +61,7 @@ func routeRequest(ctx context.Context, connCtx connection.Context, req message.R
 		return request.HandleGetFileRequest(req), nil
 	case message.PutFileRequestType:
 		req := req.(message.PutFileRequest)
-		return request.HandlePutFileRequest(ctx, connCtx, req)
+		return request.HandlePutFileRequest(ctx, serverRouter.Context, req)
 	case message.DeleteFileRequestType:
 		req := req.(message.DeleteFileRequest)
 		return request.HandleDeleteFileRequest(req)
@@ -66,40 +70,39 @@ func routeRequest(ctx context.Context, connCtx connection.Context, req message.R
 	}
 }
 
-func deliverResponse(ctx context.Context, connCtx connection.Context, res message.Response) error {
+func (serverRouter ServerRouter) deliverResponse(ctx context.Context, res message.Response) error {
 	ctx, cancel := context.WithTimeout(ctx, timeForRequest)
 	defer cancel()
 
 	switch res.GetType() {
 	case message.GetFileResponseType:
 		res := res.(decorated.GetFileResponse)
-		return sendGetFileResponse(ctx, connCtx, res)
+		return serverRouter.sendGetFileResponse(ctx, res)
 	default:
-		return sendResponse(connCtx, res)
+		return serverRouter.sendResponse(res)
 	}
 }
 
-func sendGetFileResponse(ctx context.Context, connCtx connection.Context, res decorated.GetFileResponse) error {
+func (serverRouter ServerRouter) sendGetFileResponse(ctx context.Context, res decorated.GetFileResponse) error {
 	f, err := os.Open(res.FileName)
 	if errors.Is(err, os.ErrNotExist) {
-		return sendNotFoundResponse(connCtx, res)
+		return serverRouter.sendNotFoundResponse(res)
 	} else if err != nil {
 		return err
 	}
 	defer file.Close(f)
 
-	return streamFileResponse(ctx, connCtx, f, res)
+	return serverRouter.streamFileResponse(ctx, f, res)
 }
 
-func streamFileResponse(
+func (serverRouter ServerRouter) streamFileResponse(
 	ctx context.Context,
-	connCtx connection.Context,
 	f *os.File,
 	res decorated.GetFileResponse,
 ) error {
-	var writer io.Writer = connCtx.Conn
-	headerBuffer := connCtx.HeaderBuffer
-	messageBuffer := connCtx.Buffer
+	var writer io.Writer = serverRouter.Conn
+	headerBuffer := serverRouter.HeaderBuffer
+	messageBuffer := serverRouter.Buffer
 
 	fileSize, err := file.GetSize(f)
 	if err != nil {
@@ -113,17 +116,17 @@ func streamFileResponse(
 	return transfer.Stream(ctx, f, writer, messageBuffer, res.Size)
 }
 
-func sendNotFoundResponse(connCtx connection.Context, res decorated.GetFileResponse) error {
+func (serverRouter ServerRouter) sendNotFoundResponse(res decorated.GetFileResponse) error {
 	res.GetFileResponse.Status = http.StatusNotFound
 	res.Size = 0
 
-	return sendResponse(connCtx, res)
+	return serverRouter.sendResponse(res)
 }
 
-func sendResponse(connCtx connection.Context, res message.Response) error {
-	var writer io.Writer = connCtx.Conn
-	headerBuffer := connCtx.HeaderBuffer
-	messageBuffer := connCtx.Buffer
+func (serverRouter ServerRouter) sendResponse(res message.Response) error {
+	var writer io.Writer = serverRouter.Conn
+	headerBuffer := serverRouter.HeaderBuffer
+	messageBuffer := serverRouter.Buffer
 	return transfer.SendMessage(writer, headerBuffer, messageBuffer, res)
 }
 
