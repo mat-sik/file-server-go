e3a27518a8619b6262c9ccbc947264573ba1b7d9
Mateusz <msikorski@griddynamics.com>
2025-01-03 10:51:48 +0100

    Add code to test every endpoint

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -11,9 +11,33 @@ func main() {
 		panic(err)
 	}
 
-	req := &message.GetFileRequest{FileName: "foo.txt"}
+	getFileReq := &message.GetFileRequest{FileName: "foo.txt"}
 
-	err = webClient.Run(req)
+	err = webClient.Run(getFileReq)
+	if err != nil {
+		panic(err)
+	}
+
+	webClient, err = client.NewClient(":44696")
+	if err != nil {
+		panic(err)
+	}
+
+	delFileReq := &message.DeleteFileRequest{FileName: "foo.txt"}
+
+	err = webClient.Run(delFileReq)
+	if err != nil {
+		panic(err)
+	}
+
+	webClient, err = client.NewClient(":44696")
+	if err != nil {
+		panic(err)
+	}
+
+	putFileReq := &message.PutFileRequest{FileName: "foo.txt"}
+
+	err = webClient.Run(putFileReq)
 	if err != nil {
 		panic(err)
 	}
