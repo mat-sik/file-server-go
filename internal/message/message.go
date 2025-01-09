ef02edfe6b53932243abc32dd808b0b12b3e17d7
Mateusz <siiikor.sikorski@gmail.com>
2025-01-09 20:59:14 +0100

    Improve message, request, response interfaces by following the style from protobuf.

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,64 +1,10 @@
 package message
 
-import (
-	"errors"
-)
-
-type Message interface {
-	Type() TypeName
-}
-
-type Request interface {
-	Message
-}
-
-type Response interface {
-	Message
-}
-
-type TypeName uint64
-
-const (
-	GetFileRequestType TypeName = iota
-	GetFileResponseType
-
-	PutFileRequestType
-	PutFileResponseType
-
-	DeleteFileRequestType
-	DeleteFileResponseType
-)
-
-func TypeNameConverter(typeName TypeName) (Message, error) {
-	switch typeName {
-	case GetFileRequestType:
-		return &GetFileRequest{}, nil
-	case GetFileResponseType:
-		return &GetFileResponse{}, nil
-	case PutFileRequestType:
-		return &PutFileRequest{}, nil
-	case PutFileResponseType:
-		return &PutFileResponse{}, nil
-	case DeleteFileRequestType:
-		return &DeleteFileRequest{}, nil
-	case DeleteFileResponseType:
-		return &DeleteFileResponse{}, nil
-	default:
-		return nil, errors.New("unknown type")
-	}
-}
-
 type GetFileRequest struct {
-	Request
 	FileName string
 }
 
-func (req GetFileRequest) Type() TypeName {
-	return GetFileRequestType
-}
-
 type GetFileResponse struct {
-	Response
 	Status int
 	Size   int
 }
@@ -67,22 +13,12 @@ func NewGetFileResponse(status int, size int) *GetFileResponse {
 	return &GetFileResponse{Status: status, Size: size}
 }
 
-func (res GetFileResponse) Type() TypeName {
-	return GetFileResponseType
-}
-
 type PutFileRequest struct {
-	Request
 	FileName string
 	Size     int
 }
 
-func (req PutFileRequest) Type() TypeName {
-	return PutFileRequestType
-}
-
 type PutFileResponse struct {
-	Response
 	Status int
 }
 
@@ -90,21 +26,11 @@ func NewPutFileResponse(status int) *PutFileResponse {
 	return &PutFileResponse{Status: status}
 }
 
-func (res PutFileResponse) Type() TypeName {
-	return PutFileResponseType
-}
-
 type DeleteFileRequest struct {
-	Request
 	FileName string
 }
 
-func (req DeleteFileRequest) Type() TypeName {
-	return DeleteFileRequestType
-}
-
 type DeleteFileResponse struct {
-	Response
 	Status int
 }
 
@@ -112,6 +38,52 @@ func NewDeleteFileResponse(status int) *DeleteFileResponse {
 	return &DeleteFileResponse{Status: status}
 }
 
-func (res DeleteFileResponse) Type() TypeName {
-	return DeleteFileResponseType
+type Message interface {
+	isMessage()
+}
+
+func (_ GetFileRequest) isMessage() {
+}
+
+func (_ GetFileResponse) isMessage() {
+}
+
+func (_ PutFileRequest) isMessage() {
+}
+
+func (_ PutFileResponse) isMessage() {
+}
+
+func (_ DeleteFileRequest) isMessage() {
+}
+
+func (_ DeleteFileResponse) isMessage() {
+}
+
+type Request interface {
+	isMessage()
+	isRequest()
+}
+
+func (_ GetFileRequest) isRequest() {
+}
+
+func (_ PutFileRequest) isRequest() {
+}
+
+func (_ DeleteFileRequest) isRequest() {
+}
+
+type Response interface {
+	isMessage()
+	isResponse()
+}
+
+func (_ GetFileResponse) isResponse() {
+}
+
+func (_ PutFileResponse) isResponse() {
+}
+
+func (_ DeleteFileResponse) isResponse() {
 }
