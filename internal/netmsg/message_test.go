bd42eb12a97a8d392fa32b3ea9fb0fa4eda80182
Mateusz <siiikor.sikorski@gmail.com>
2025-03-08 16:04:45 +0100

    Improve the test for sending and receving messages

--- internal/netmsg/message_test.go
+++ internal/netmsg/message_test.go
@@ -2,38 +2,47 @@ package netmsg
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/mat-sik/file-server-go/internal/message"
+	"reflect"
 	"testing"
 )
 
-func Test_SendMessage_And_ReceiveMessage(t *testing.T) {
-	in := message.PutFileRequest{FileName: "huge_file_name", Size: 404}
-	buffer := make([]byte, 1024)
-	bytesBuffer := bytes.NewBuffer(make([]byte, 0, 1024))
-
-	readWriteCloser := &mockReadWriteCloser{Buffer: *bytesBuffer}
-
-	messageDispatcher := Session{
-		Conn:   readWriteCloser,
-		Buffer: buffer,
+func Test_should_SendMessage_And_ReceiveIt(t *testing.T) {
+	testCases := []struct {
+		name    string
+		message message.Message
+	}{
+		{name: "PUT File Request", message: message.PutFileRequest{FileName: "foo.txt", Size: 404}},
+		{name: "PUT File Response", message: message.PutFileResponse{Status: 200}},
+		{name: "GET File Request", message: message.GetFileRequest{FileName: "foo.txt"}},
+		{name: "GET File Response", message: message.GetFileResponse{Status: 200, Size: 404}},
+		{name: "DELETE File Request", message: message.DeleteFileRequest{FileName: "foo.txt"}},
+		{name: "DELETE File Response", message: message.DeleteFileResponse{Status: 200}},
 	}
 
-	err := messageDispatcher.SendMessage(in)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			buffer := make([]byte, 1024)
+			readWriteCloser := &mockReadWriteCloser{Buffer: *bytes.NewBuffer(make([]byte, 0, 1024))}
 
-	out, err := messageDispatcher.ReceiveMessage()
-	if err != nil {
-		t.Fatal(err)
-	}
+			session := Session{
+				Conn:   readWriteCloser,
+				Buffer: buffer,
+			}
 
-	switch v := out.(type) {
-	case message.PutFileRequest:
-		fmt.Printf("%v", v)
-	default:
-		t.Errorf("%v", v)
+			if err := session.SendMessage(tc.message); err != nil {
+				t.Fatal(err)
+			}
+
+			out, err := session.ReceiveMessage()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(out, tc.message) {
+				t.Fatalf("got %T of value %v, want %T of value %v", out, out, tc.message, tc.message)
+			}
+		})
 	}
 }
 
