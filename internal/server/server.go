7d3acb5c08ee58dbb3ef1344e73aeab3185fe48d
Mateusz <siiikor.sikorski@gmail.com>
2025-03-09 13:07:21 +0100

    Implement some integration tests.

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"sync"
 )
 
 func Run(ctx context.Context, addr string) error {
@@ -16,39 +17,60 @@ func Run(ctx context.Context, addr string) error {
 	if err != nil {
 		return err
 	}
-	defer safeListenerClose(listener)
+	defer files.LoggedClose(listener)
 
+	return run(ctx, listener)
+}
+
+func runWithWaitGroup(ctx context.Context, wg *sync.WaitGroup, addr string) error {
+	listener, err := net.Listen("tcp4", addr)
+	if err != nil {
+		return err
+	}
+	defer files.LoggedClose(listener)
+	wg.Done()
+
+	return run(ctx, listener)
+}
+
+func run(ctx context.Context, listener net.Listener) error {
 	connCh := make(chan net.Conn)
 	errCh := make(chan error)
 
 	go acceptConnections(listener, connCh, errCh)
 
+	return connectionLoop(ctx, connCh, errCh)
+}
+
+func connectionLoop(ctx context.Context, connCh <-chan net.Conn, errCh chan error) error {
 	for {
 		select {
 		case conn := <-connCh:
 			go handleRequest(ctx, conn, errCh)
-		case err = <-errCh:
+		case err := <-errCh:
 			if !errors.Is(err, io.EOF) {
 				return err
 			}
 			slog.Info("Connection closed from client")
+		case <-ctx.Done():
+			return nil
 		}
 	}
 }
 
 func acceptConnections(listener net.Listener, connCh chan<- net.Conn, errCh chan<- error) {
 	for {
-		if conn, err := listener.Accept(); err != nil {
+		conn, err := listener.Accept()
+		if err != nil {
 			errCh <- err
 			return
-		} else {
-			connCh <- conn
 		}
+		connCh <- conn
 	}
 }
 
 func handleRequest(ctx context.Context, conn net.Conn, errCh chan<- error) {
-	defer safeConnectionClose(conn)
+	defer files.LoggedClose(conn)
 
 	session := netmsg.NewSession(conn)
 
@@ -66,16 +88,4 @@ func handleRequest(ctx context.Context, conn net.Conn, errCh chan<- error) {
 	}
 
 	errCh <- err
-}
-
-func safeConnectionClose(conn net.Conn) {
-	if err := conn.Close(); err != nil {
-		panic(err)
-	}
-}
-
-func safeListenerClose(listener net.Listener) {
-	if err := listener.Close(); err != nil {
-		panic(err)
-	}
 }
---
+++ internal/server/server_test.go
@@ -0,0 +1,257 @@
+package server
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/mat-sik/file-server-go/internal/client"
+	"github.com/mat-sik/file-server-go/internal/envs"
+	"github.com/mat-sik/file-server-go/internal/files"
+	"github.com/mat-sik/file-server-go/internal/message"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	setUpTestEnvs()
+	setUpTestDirs()
+	defer cleanTestDirs()
+
+	m.Run()
+}
+
+func Test_shouldGetFileFromServer(t *testing.T) {
+	filename := "getFileTest.txt"
+	serverFilePath := filepath.Join(testServerStoragePath, filename)
+	createFile(serverFilePath, 1024*1024)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// when
+	go runServer(ctx, wg)
+	wg.Wait()
+
+	// and when
+	webClient := getClient()
+
+	putFileReq := message.GetFileRequest{FileName: filename}
+	err := webClient.Run(putFileReq)
+
+	// then
+	if err != nil {
+		t.Fatal(err)
+	}
+	clientFilePath := filepath.Join(testClientStoragePath, filename)
+	if !filesEqual(clientFilePath, serverFilePath) {
+		t.Fatalf("file not equal")
+	}
+}
+
+func Test_shouldPutFileToServer(t *testing.T) {
+	filename := "putFileTest.txt"
+	clientFilePath := filepath.Join(testClientStoragePath, filename)
+	createFile(clientFilePath, 1024*1024)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// when
+	go runServer(ctx, wg)
+	wg.Wait()
+
+	// and when
+	webClient := getClient()
+
+	putFileReq := message.PutFileRequest{FileName: filename}
+	err := webClient.Run(putFileReq)
+
+	// then
+	if err != nil {
+		t.Fatal(err)
+	}
+	serverFilePath := filepath.Join(testServerStoragePath, filename)
+	if !filesEqual(serverFilePath, clientFilePath) {
+		t.Fatalf("file not equal")
+	}
+}
+
+func Test_shouldDeleteFileFromServer(t *testing.T) {
+	filename := "deleteFileTest.txt"
+	serverFilePath := filepath.Join(testServerStoragePath, filename)
+	createFile(serverFilePath, 1024*1024)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// when
+	go runServer(ctx, wg)
+	wg.Wait()
+
+	// and when
+	webClient := getClient()
+
+	putFileReq := message.DeleteFileRequest{FileName: filename}
+	err := webClient.Run(putFileReq)
+
+	// then
+	if err != nil {
+		t.Fatal(err)
+	}
+	if fileExists(serverFilePath) {
+		t.Fatalf("file exists, but should have been deleted")
+	}
+}
+
+func runServer(ctx context.Context, wg *sync.WaitGroup) {
+	addr := fmt.Sprintf(":%d", port)
+
+	if err := runWithWaitGroup(ctx, wg, addr); err != nil {
+		panic(err)
+	}
+}
+
+func getClient() client.Client {
+	addr := fmt.Sprintf(":%d", port)
+
+	webClient, err := client.NewClient(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	return webClient
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	if errors.Is(err, fs.ErrNotExist) {
+		return false
+	}
+	if err != nil {
+		panic(err)
+	}
+	return true
+}
+
+func filesEqual(firstPath string, secondPath string) bool {
+	firstFile, err := os.Open(firstPath)
+	if err != nil {
+		panic(err)
+	}
+	defer files.LoggedClose(firstFile)
+	secondFile, err := os.Open(secondPath)
+	if err != nil {
+		panic(err)
+	}
+	defer files.LoggedClose(secondFile)
+
+	return fileLengthEqual(firstFile, secondFile) && fileContentsEqual(firstFile, secondFile)
+}
+
+func fileContentsEqual(firstFile *os.File, secondFile *os.File) bool {
+	firstBuffer := copyBuffer[:len(copyBuffer)/2]
+	secondBuffer := copyBuffer[len(copyBuffer)/2:]
+
+	for {
+		firstN, firstErr := firstFile.Read(firstBuffer)
+		secondN, secondErr := secondFile.Read(secondBuffer)
+		if (firstErr != nil && secondErr == nil) || (firstErr == nil && secondErr != nil) {
+			return false
+		} else if firstErr != nil && secondErr != nil {
+			return true
+		}
+		if firstN != secondN {
+			return false
+		}
+		if !bytes.Equal(firstBuffer[:firstN], secondBuffer[:secondN]) {
+			return false
+		}
+	}
+}
+
+func fileLengthEqual(firstFile *os.File, secondFile *os.File) bool {
+	firstSize, err := files.SizeOf(firstFile)
+	if err != nil {
+		panic(err)
+	}
+	secondSize, err := files.SizeOf(secondFile)
+	if err != nil {
+		panic(err)
+	}
+
+	return firstSize == secondSize
+}
+
+func createFile(path string, size int) {
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer files.LoggedClose(file)
+
+	for i := 0; i < min(size, len(copyBuffer)); i++ {
+		copyBuffer[i] = 'x'
+	}
+
+	bytesWritten := 0
+	for bytesWritten < size {
+		remaining := size - bytesWritten
+
+		writeSize := min(remaining, len(copyBuffer))
+		n, err := file.Write(copyBuffer[:writeSize])
+		if err != nil {
+			panic(err)
+		}
+
+		bytesWritten += n
+	}
+
+	slog.Info("Created file", "path", path, "size", bytesWritten)
+}
+
+func setUpTestDirs() {
+	dirs := []string{testServerStoragePath, testClientStoragePath}
+	createDirs(dirs)
+}
+
+func createDirs(dirs []string) {
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			cleanTestDirs()
+			panic(err)
+		}
+	}
+}
+
+func cleanTestDirs() {
+	if err := os.RemoveAll(pathToTest); err != nil {
+		panic(err)
+	}
+}
+
+func setUpTestEnvs() {
+	envs.ClientStoragePath = testClientStoragePath
+	envs.ServerStoragePath = testServerStoragePath
+}
+
+var (
+	pathToRoot            = "../../"
+	pathToTest            = filepath.Join(pathToRoot, "test")
+	testServerStoragePath = filepath.Join(pathToTest, "server/storage")
+	testClientStoragePath = filepath.Join(pathToTest, "client/storage")
+
+	port = 33303
+
+	copyBuffer = make([]byte, 64*1024)
+)
